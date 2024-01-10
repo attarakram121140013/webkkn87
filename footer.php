6c0dd53a5f2c2dde4adc86739559c1c5b908b973
Attar Akram Abdillah <127123155+attarakram121140013@users.noreply.github.com>
2024-01-11 00:37:24 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,28 @@
+<div class="container-fluid py-5 content-subscribe text-light">
+    <div class="container">
+        <h5 class="text-center mb-4">Temui Kami</h5>
+        <div class="row justify-content-center">
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="..."><i class="fa-brands fa-facebook"></i></a>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="..."><i class="fa-brands fa-instagram"></i></a>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <a href="..."><i class="fa-brands fa-twitter"></i></a>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center">
+                <a href="..."><i class="fa-brands fa-youtube"></i></a>
+            </div>
+            <div class="col-sm-1 d-flex justify-content-center">
+                <a href="..."><i class="fa-brands fa-whatsapp"></i></a>
+            </div>
+    </div>
+</div>
+
+<div class="container-fluid py-3 bg-dark text-light">
+    <div class="container d-flex justify-content-between">
+        <label>&copy;2023 KKN ITERA</label>
+        <label>Created by Kelompok 87</label>
+    </div>
+</div>
---
+++ tentang-kami.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/fontawesome-free-6.4.2-web/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>KKN ITERA 87 | Tentang Kami</title>
+</head>
+<body>
+    <?php require "navbar.php"; ?>
+
+    <div class="container-fluid banner2 d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center"> Tentang Kami</h1>
+        </div>
+    </div>
+    <div class="container-fluid py-5">
+            <div class="container fs-5 text-center">
+                <p>
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                </p>
+                <p>
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar Los penguinos de la mascar 
+                </p>
+            </div>
+        </div>
+
+        <?php require "footer.php"; ?>
+        
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/fontawesome-free-6.4.2-web/js/all.min.js"></script>
+</body>
+</html>
